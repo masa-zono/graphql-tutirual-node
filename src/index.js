db34069062a85a9abc6bde331a79cf2966c45894
satooookibi <masa.zin9@gmail.com>
2021-04-05 23:00:43 +0900

    Realtime GraphQL Subscriptions

--- src/index.js
+++ src/index.js
@@ -5,15 +5,22 @@ const Query = require("./resolvers/Query");
 const Mutation = require("./resolvers/Mutation");
 const User = require("./resolvers/User");
 const Link = require("./resolvers/Link");
+const Subscription = require("./resolvers/Subscription");
+const Vote = require("./resolvers/Vote");
+
 const fs = require("fs");
 const path = require("path");
+const { PubSub } = require("apollo-server");
+const pubsub = new PubSub();
 const prisma = new PrismaClient();
 
 const resolvers = {
   Query,
   Mutation,
+  Subscription,
   User,
   Link,
+  Vote,
 };
 
 const server = new ApolloServer({
@@ -23,6 +30,7 @@ const server = new ApolloServer({
     return {
       ...req,
       prisma,
+      pubsub,
       userId: req && req.headers.authorization ? getUserId(req) : null,
     };
   },
